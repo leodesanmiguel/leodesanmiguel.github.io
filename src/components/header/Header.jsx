35def94e38a3c9edcf01893d0d7303e9eb66ec96
Leo Marti <leodesanmiguel@gmail.com>
2023-08-06 21:54:49 -0300

    work-1: Fix Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,12 +3,10 @@ import { Login, Logo } from "./Components";
 import Imagen from "../../assets/logo_my_tinerary.png";
 import { MenuBar } from "./MenuBar";
 import {
-  //  Button,
-  //  Card,
+  Button,
   IconButton,
+  MobileNav,
   Navbar,
-  //  MobileNav,
-  //  Navbar,
   Typography,
 } from "@material-tailwind/react";
 
@@ -21,70 +19,145 @@ export const Header = () => {
 
   const navList = (
     <>
-    <div className="flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6" >
-      <MenuBar />
-      <Login />
-    </div>
+      <div className="flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+        <MenuBar />
+        <Login />
+      </div>
     </>
   );
 
   return (
     <>
-    <Navbar className="mx-auto max-w-screen-xl py-1 px-1 ">
-      <div className="space-y-2 p-4 sm:px-8 sm:py-6 lg:p-4 xl:px-8 xl:py-6">
-        <div className="flex items-center justify-between text-blue-gray-900">
-          <Typography
-            as="a"
-            href="#"
-            className="mr-4 cursor-pointer py-1 font-medium"
-          >
-            <Logo imagen={Imagen} />
-          </Typography>
-          <div className="hidden lg:block">
-            {navList}
-
-            <IconButton
-              variant="text"
-              className="ml-auto hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-              ripple={false}
-              onClick={() => setOpenNav(!openNav)}
+      <Navbar className="mx-auto max-w-screen-xl py-1 px-1 ">
+        <div className="space-y-2 p-4 sm:px-8 sm:py-6 lg:p-4 xl:px-8 xl:py-6">
+          <div className="flex items-center justify-between  text-blue-gray-900">
+            <Typography
+              as="a"
+              href="#"
+              className="mr-4 cursor-pointer py-1 font-medium"
             >
-              {openNav ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  className="h-6 w-6"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              )}
-            </IconButton>
+              <Logo imagen={Imagen} />
+            </Typography>
+            <div className="hidden lg:block">
+              {navList}
+
+              <IconButton
+                variant="text"
+                className="ml-auto 
+                hover:bg-transparent 
+                focus:bg-transparent 
+                active:bg-transparent 
+                lg:hidden"
+                ripple={false}
+                onClick={() => setOpenNav(!openNav)}
+              >
+                {openNav ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    className="h-6 w-6"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={3}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth={3}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                )}
+              </IconButton>
+            </div>
           </div>
         </div>
-      </div>
+          <MobileNav open={openNav}>
+            <div className="container mx-auto">
+              {navList}
+              {/* <Button variant="gradient" size="sm" fullWidth className="mb-2">
+                <span>Buy Now</span>
+              </Button> */}
+
+            </div>
+          </MobileNav>
       </Navbar>
+
+      (
+    <Navbar className="mx-auto max-w-screen-xl py-2 px-4 lg:px-8 lg:py-4">
+      <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
+        <Typography
+          as="a"
+          href="#"
+          className="mr-4 cursor-pointer py-1.5 font-medium"
+        >
+          Material Tailwind
+        </Typography>
+        <div className="hidden lg:block">{navList}</div>
+        <Button variant="gradient" size="sm" className="hidden lg:inline-block">
+          <span>Buy Now</span>
+        </Button>
+        <IconButton
+          variant="text"
+          className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+          ripple={false}
+          onClick={() => setOpenNav(!openNav)}
+        >
+          {openNav ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              className="h-6 w-6"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          )}
+        </IconButton>
+      </div>
+      <MobileNav open={openNav}>
+        <div className="container mx-auto">
+          {navList}
+          {/* <Button variant="gradient" size="sm" fullWidth className="mb-2">
+            <span>Buy Now</span>
+          </Button> */}
+        </div>
+      </MobileNav>
+    </Navbar>
+  );
     </>
   );
-
 };
