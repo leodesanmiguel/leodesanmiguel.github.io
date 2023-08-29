be7660ee9397a16998a93269940eca2f2a07009d
Leo Marti <leodesanmiguel@gmail.com>
2023-08-29 18:56:23 -0300

    SPRINT-3: redux1

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,48 @@
 import "./App.css";
-import {createBrowserRouter, RouterProvider} from 'react-router-dom';
-import Home from './pages/home/Home';
-import Cities from './pages/cities/Cities';
-import AboutUs from './pages/about-us/AboutUs';
-import Login from './pages/login/Login';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Cities from "./pages/cities/Cities";
+import AboutUs from "./pages/about-us/AboutUs";
+import Login from "./pages/login/Login";
 import LayoutMain from "./layouts/LayoutMain";
+import { useSelector } from "react-redux";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <LayoutMain/>,
+    path: "/",
+    element: <LayoutMain />,
     children: [
       {
-        path: '/home',
-        element: <Home />
+        path: "/home",
+        element: <Home />,
       },
       {
-        path: '/cities',
-        element: <Cities />
+        path: "/cities",
+        element: <Cities />,
       },
       {
-        path: '/about-us',
-        element: <AboutUs/>
+        path: "/about-us",
+        element: <AboutUs />,
       },
       {
-        path: '/login',
-        element: <Login/>
+        path: "/login",
+        element: <Login />,
       },
-    ]
+    ],
   },
- 
-])
+]);
+
 
 function App() {
+  const { counter } = useSelector((state) => state.counter);
+
   return (
-    <RouterProvider router={router} />
+    <>
+      <RouterProvider router={router} />
+      <button type="button" onClick={()=>{}}>
+        count is: {counter}
+      </button>
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { ThemeProvider } from '@material-tailwind/react'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { ThemeProvider } from "@material-tailwind/react";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./store";
+ 
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
-    <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ThemeProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
