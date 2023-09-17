1ecac9b1aa80a2dc1999f28763c66e97ec16b50f
Leo Marti <leodesanmiguel@gmail.com>
2023-09-17 20:12:50 -0300

    DESA: Acceso hacia banckend en Render

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
-// eslint-disable-next-line no-undef
-const apiUrl = process.env.REACT_APP_API_URL;
 
+// const apiUrl = process.env.REACT_APP_API_URL;
+// const apiUrl = process.env.URL_API_CITY || 'http://localhost:3001/api';
+// const apiUrl =  'http://localhost:3001/api';
+const apiUrl =  'https://my-tinerary-mern-2023.onrender.com/api';
+ 
 // Permite crear una instancia de axios
 export const urlApi = axios.create({
     baseURL: apiUrl
