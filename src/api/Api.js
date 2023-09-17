0f8b3ac50c0724bd3efbc3d3c2e640c96e11ce9f
Leo Marti <leodesanmiguel@gmail.com>
2023-09-17 19:42:03 -0300

    DESA: variables de entorno

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
+// eslint-disable-next-line no-undef
+const apiUrl = process.env.REACT_APP_API_URL;
+
 // Permite crear una instancia de axios
 export const urlApi = axios.create({
-    baseURL: 'http://localhost:3001/api'
+    baseURL: apiUrl
 });
